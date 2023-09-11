65a1e98fb92ecf4006e2056f5f7e2b306adfa766
Kevin.P <kevin.pellissard@gmail.com>
2023-09-11 21:44:27 +0200

    Added left and right newnode. Remove function.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,10 +17,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (newnode == NULL)
 		return (NULL);
-		
-	binary_tree_node(newnode, value);
-	newnode->parent = parent;
+
 	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
 	if (parent->left != NULL)
 	{
